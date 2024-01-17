84c0ec74edbe2563b24d2bb3b089f464afd2adac
Charles Chitty <charlescchitty@gmail.com>
2024-01-17 15:20:33 -0800

    updated to-do list

--- src/App.js
+++ src/App.js
@@ -11,3 +11,9 @@ function App() {
 }
 
 export default App;
+
+
+//TODO: Winning/Losing messages + end game feature (still guess after "won")
+// We need to have a different word/changing word (not apple)
+// array of pictures; stop incrementing once snowman melts
+// Add number of wrong guesses
