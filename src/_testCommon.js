615000515a1511f7002b57871b3b7e79b5cb9022
Victor Kim <vskimjr@gmail.com>
2024-01-17 18:50:46 -0500

    finished part 0, wrote preliminary tests for game ending on a loss, figuring out how to render Snowman component with nWrong = 6

---
+++ src/_testCommon.js
@@ -0,0 +1,28 @@
+const TEST_IMAGES=[
+  {
+    src: "test1.com",
+    caption: "testing image 1"
+  },
+  {
+    src: "test2.com",
+    caption: "testing image 2"
+  },
+  {
+    src: "test3.com",
+    caption: "testing image 3"
+  },
+  {
+    src: "test4.com",
+    caption: "testing image 4"
+  },
+  {
+    src: "test5.com",
+    caption: "testing image 5"
+  },
+  {
+    src: "test6.com",
+    caption: "testing image 6"
+  },
+];
+
+export default TEST_IMAGES
